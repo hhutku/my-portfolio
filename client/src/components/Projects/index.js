75b5b36c5c02c093fb65b6a290d2ba65dca86e1e
hhutku <hhutku.wa@gmail.com>
2021-03-07 23:58:11 -0800

    components projects added

---
+++ client/src/components/Projects/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Projects = (props) => {
+    return (
+        <div >
+ {props.children}
+        </div>
+    )
+
+
+}
+
+export default Projects
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
-
+import myworks from '../portfolio.json'
+import Projects from '../components/Projects'
 const Portfolio = () => {
     return (
         <div>
-        portfolio
+           <Projects>
+               
+            {myworks.map((project) => (
+         <div> 
+         <p key={project.id} >{project.id} </p>
+         <p>{project.name} </p>
+         <p>{project.deploy} </p>
+         <p>{project.topics}</p>
+         </div>
+            ))}
+            
+            </Projects>
         </div>
     )
 }

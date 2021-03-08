4617be2863c04adcd639cc81a1184c3ea7557cd0
hhutku <hhutku.wa@gmail.com>
2021-03-08 00:23:26 -0800

    projectCards update

--- client/src/components/ProjectCards/index.js
+++ client/src/components/ProjectCards/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {images} from '../../images/images'
-const ProjectCards = ({ project }) => {
+
+const ProjectCards = ({project}) => {
 
   return (
+    
+      <div  className="col-lg-4 col-md-6 mt-4 cards "    >
+        <div className="cardContainer" >
+       
+        <img style={{ height: 300 }} src={images[parseInt(project.id)]}   className="card-img-top" alt={project.name} />
+        <div  className="card-body ">
 
-    <div className="col-lg-4 "   >
-      <img src={images[parseInt(project.id)]}   className="card-img-top" alt={project.name} />
-      <p>{project.name}</p>
-      <p>{project.id}</p>
-      <p>{project.topics}</p>
-      <p>{project.deploy}</p>
-<hr></hr>
-<br/>
-    </div>
+          <a href={project.deploy}><img src="https://img.icons8.com/color/48/000000/heroku.png" alt="Deployed Site" id="project-icon" /><span className="card-title ">{project.name}</span></a>
+          <br />
+          <br />
+          <p>{project.topics}</p>
+
+        </div>
+        </div>
+      </div>
+     
+   
+
   )
 }
 

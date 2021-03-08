90442d237f5a0475de3895abfc68c7a6d298bbe2
hhutku <hhutku.wa@gmail.com>
2021-03-08 00:12:41 -0800

    images.js for projectCards

--- client/src/components/ProjectCards/index.js
+++ client/src/components/ProjectCards/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import {images} from '../../images/images'
 const ProjectCards = ({ project }) => {
 
   return (
 
     <div className="col-lg-4 "   >
+      <img src={images[parseInt(project.id)]}   className="card-img-top" alt={project.name} />
       <p>{project.name}</p>
       <p>{project.id}</p>
       <p>{project.topics}</p>
---
+++ client/src/images/images.js
@@ -0,0 +1,15 @@
+import BestBurger from './bestBurger.jpg'
+import NoteTaker from './note-taker.png'
+import BookClub from './bookClub.jpg'
+import workDaySchedular from './workDaySchedular.png'
+import weather from './weather.png'
+import space from './space.jpg'
+import quizChallenge from './quizChallenge.png'
+import passGen from './passGen.png'
+import employee from './employee.jpg'
+import photo from './foto.png'
+
+export const myPhoto =[photo]
+export const images=[BookClub,BestBurger,employee,space,weather,workDaySchedular,quizChallenge,passGen,NoteTaker]
+
+

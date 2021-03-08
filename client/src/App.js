a0514c4f58df4d68153cabe396f598f5dce54db8
hhutku <hhutku.wa@gmail.com>
2021-03-07 21:50:06 -0800

    App update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Portfolio from './pages/Portfolio'
+import About from './pages/About'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
 
+import './App.css'
 function App() {
   return (
-    <div className="App">
-my portfolio
+<div className="myApp">
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path= "/portfolio" >
+            <Portfolio  />
+          </Route>
+          <Route  exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/resume">
+            <Resume />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route >
+            <Portfolio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+   
     </div>
   );
 }
-
 
 export default App;

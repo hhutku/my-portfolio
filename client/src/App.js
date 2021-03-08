5b22f5fc7af080f892570be63fa5ede46ce766e8
hhutku <hhutku.wa@gmail.com>
2021-03-07 22:01:30 -0800

    pages folder added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react'
 import Portfolio from './pages/Portfolio'
 import About from './pages/About'
 import Resume from './pages/Resume'
@@ -7,29 +8,29 @@ import Contact from './pages/Contact'
 import './App.css'
 function App() {
   return (
-<div className="myApp">
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path= "/portfolio" >
-            <Portfolio  />
-          </Route>
-          <Route  exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/resume">
-            <Resume />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          <Route >
-            <Portfolio />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-   
+    <div className="myApp">
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/portfolio" >
+              <Portfolio />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/resume">
+              <Resume />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <Route >
+              <Portfolio />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
     </div>
   );
 }

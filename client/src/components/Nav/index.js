efde540853ec10c9ef000bd029ac05b8994a7019
hhutku <hhutku.wa@gmail.com>
2021-05-29 11:37:13 -0700

    nav update

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,30 +1,64 @@
 import React from 'react'
 import './style.css'
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap'
 
-
-
-
+import { Link, useLocation } from 'react-router-dom'
 
 const MyNav = () => {
+  const location = useLocation()
   return (
-<Navbar className="navbar navbar-expand-lg navbar-dark bg-dark" expand="lg">
-  <Navbar.Brand href="/"> <a className="navbar-brand" href="/"><img src="https://img.icons8.com/color/48/000000/react-native.png" alt="react logo" className="icon"/></a></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-    <Nav.Link href="/about">About</Nav.Link>
-    <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-    <Nav.Link href="/contact">Contact</Nav.Link>
-     <Nav.Link href="/resume">Resume</Nav.Link>
-     
+    <Navbar className='navbar navbar-expand-lg navbar-dark bg-dark' expand='lg'>
+      <Link
       
-     
- 
-    </Nav>
-
-  </Navbar.Collapse>
-</Navbar>
+      to='/'>
+        {' '}
+        <a className='navbar-brand' href='/'>
+          <img
+            src='https://img.icons8.com/color/48/000000/react-native.png'
+            alt='react logo'
+            className='icon'
+          />
+        </a>
+      </Link>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+        <Link
+            to='/about'
+            className={
+              location.pathname === '/about' ||  location.pathname === '/' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            About
+          </Link>
+         
+          <Link
+            to='/portfolio'
+            className={
+              location.pathname === '/portfolio' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Portfolio
+          </Link>
+          <Link
+            to='/contact'
+            className={
+              location.pathname === '/contact' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Contact
+          </Link>
+          <Link
+            to='/resume'
+            className={
+              location.pathname === '/resume' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Resume
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
